10733bac8a5fb3d255f44cc4a28bdbd796a85f3c
Eesham Kohli <eeshamkohli23@gmail.com>
2018-10-23 00:55:31 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './Home/header/header.component';
+import {Routes, RouterModule} from '@angular/router';
+import { FooterComponent } from './home/footer/footer.component';
 
+const routes : Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'signup',component:SignupComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
